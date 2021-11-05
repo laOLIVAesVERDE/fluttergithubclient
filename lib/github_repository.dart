5ea70dad0d3453d9919bdf1ab1423596b873f92e
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-11-05 23:01:09 +0900

    Create github_repository.dart

---
+++ lib/github_repository.dart
@@ -0,0 +1,18 @@
+class GithubRepository {
+  final String fullName;
+  final String description;
+  final String language;
+  final String htmlUrl;
+  final int stargazersCount;
+  final int watchersCount;
+  final int forksCount;
+
+  GithubRepository.fromJson(Map<String, dynamic> json)
+  : fullName = json["full_name"],
+  description = json["description"],
+  language = json["language"],
+  htmlUrl = json["html_url"],
+  stargazersCount = json["stargazers_count"],
+  watchersCount = json["watchers_count"],
+  forksCount = json["forks_count"];
+}
