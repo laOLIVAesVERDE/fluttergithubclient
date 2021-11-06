0eca12e96fb49f173f74609123962e0868cbab74
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-11-07 02:37:41 +0900

    Update api_client.dart

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -1,5 +1,8 @@
 
+import 'dart:convert';
+
 import 'package:fluttergithubclient/github_repository.dart';
+import 'package:http/http.dart' as http;
 
 class ApiClient {
   static final ApiClient _instance = ApiClient._privateConstructor();
@@ -7,6 +10,16 @@ class ApiClient {
   static ApiClient get instance => _instance;
 
   Future<List<GithubRepository>> _searchRepositories(String searchWord) async {
-    final response = await http.get()
+    final response = await http.get('https://api.github.com/search/repositories?q=' + searchWord + '&sort=stars&order=desc');
+    if (response.statusCode == 200) {
+      List<GithubRepository> list = [];
+      Map<String, dynamic> decoded = json.decode(response.body);
+      for (var item in decoded["items"]) {
+        list.add(GithubRepository.fromJson(item));
+      }
+      return list;
+    } else {
+      throw Exception('Fail to search repository');
+    }
   }
 }
