a4586ea3a00d828b0a3aaae46a136919227b9188
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-11-07 02:31:20 +0900

    Update api_client.dart

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -1,6 +1,12 @@
 
+import 'package:fluttergithubclient/github_repository.dart';
+
 class ApiClient {
   static final ApiClient _instance = ApiClient._privateConstructor();
   ApiClient._privateConstructor();
   static ApiClient get instance => _instance;
+
+  Future<List<GithubRepository>> _searchRepositories(String searchWord) async {
+    final response = await http.get()
+  }
 }
