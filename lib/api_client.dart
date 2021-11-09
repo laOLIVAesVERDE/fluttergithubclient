68ee27e0f7c82a7bac185028a97faf76e0d498dc
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-11-10 00:57:51 +0900

    Update api_client.dart

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -10,7 +10,7 @@ class ApiClient {
   static ApiClient get instance => _instance;
 
   Future<List<GithubRepository>> searchRepositories(String searchWord) async {
-    final uri = Uri.parse("https://api.github.com/search/repositories?q=" + searchWord + '&sort=stars&order=desc');
+    final uri = Uri.parse("https://api.github.com/search/repositories?q=" + searchWord + "&sort=stars&order=desc");
     final response = await http.get(uri);
     if (response.statusCode == 200) {
       List<GithubRepository> list = [];
